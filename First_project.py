c8fc90904ea9873aee3e22f4523663fd67cb5c50
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-11-27 13:13:45 +0530

    Add files via upload

---
+++ First_project.py
@@ -0,0 +1,4 @@
+print("Welcome to coding world")
+# 102
+print(123456789101112)
+print("sahaj",123456)
---
+++ variable.py
@@ -0,0 +1,5 @@
+#we are learning variables
+chocolate="Kit kat"
+Age=9
+Name=input("enter your name:")
+print("Welcome to codingal",Name)
