93cac4837862ec0073434a0ca30fd69ed600d2f2
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-12-03 18:21:19 +0530

    Add files via upload

---
+++ even odd.py
@@ -0,0 +1,5 @@
+v1=int(input("enter a number "))
+if v1%2==0:
+    print("even")
+else:
+    print("odd")
---
+++ profit.py
@@ -0,0 +1,7 @@
+actual_price=500
+sell_price=1000
+profit=sell_price-actual_price
+if actual_price<sell_price:
+    print("profit=",profit)
+else:
+    print("loss")
