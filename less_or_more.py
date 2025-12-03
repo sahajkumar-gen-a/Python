459873bd66aa5f5c3910ee1215fbaeed58057e5b
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-12-03 18:12:02 +0530

    Add files via upload

---
+++ less_or_more.py
@@ -0,0 +1,5 @@
+v1=int(input("enter your number"))
+if v1>15:
+    print("more than 15")
+else:
+    print("less than 15")
---
+++ positive.py
@@ -0,0 +1,5 @@
+v1=int(input("enter a number"))
+if v1<0:
+    print("Negitive")
+else:
+    print("positive")
