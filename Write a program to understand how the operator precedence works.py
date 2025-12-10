3484515f77b09ead0b99f1091fc3959d86fbd8a6
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-12-10 18:20:31 +0530

    Add files via upload

---
+++ Write a program to understand how the operator precedence works.py
@@ -0,0 +1,13 @@
+#Write a program to understand how the operator precedence works
+#PEMDASS
+v1=(2+3*4)
+print (v1)
+vk44=(2*3**2)
+print (vk44)
+#/usr/local/bin/python3 "/Users/sheeljaitley/Desktop/Sahaj_Python Projects/Write a program to understand how the operator precedence works.py"
+#sheeljaitley@Sheels-MacBook-Pro Sahaj_Python Projects % /usr/local/bin/python3 "/Users/
+#sheeljaitley/Desktop/Sahaj_Python Projects/Write a program to understand how the operat
+#or precedence works.py"
+#14
+#18
+#sheeljaitley@Sheels-MacBook-Pro Sahaj_Python Projects % c
---
+++ Write to check a number is divisible by another number.py
@@ -0,0 +1,7 @@
+#Write to check a number is divisible by another number.
+n1=int(input("enter n1="))
+n2=int(input("enter n2="))
+if n1 % n2==0:
+    print("divisible")
+else:
+    print("not divisible")
