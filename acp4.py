e8d5e4cb86e43ff2632ffd60bcb353a5606bd511
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-12-20 12:13:35 +0530

    Add files via upload

---
+++ acp4.py
@@ -0,0 +1,6 @@
+month=input("enter the month ")
+month=month.lower()
+if month=="may"or month=="june"or month=="july" or month=="april"or month=="march":
+    print("summer")
+else:
+    print("Not summer")
