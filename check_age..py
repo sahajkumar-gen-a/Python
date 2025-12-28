02c2c9cb1cc818ccbfea3c6b72b634bd9581e362
sahajkumar-gen-a <sahajkumar.gena@gmail.com>
2025-12-28 12:47:20 +0530

    Add files via upload

---
+++ check_age..py
@@ -0,0 +1,6 @@
+#Write a program to check if the age entered by the user is between 10 to 20 years or not
+s=input("enter your age ")
+if s>10 and s<20:
+    print("you are a teenager")
+else:
+    print("you are not a teenager")
